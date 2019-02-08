e5878d2f4d2930344e613d49c1f41163e0f2a7a7
Vivek Pattanaik <vivekpattanaik100@gmail.com>
2019-02-08 21:39:47 +0530

    Parsing Payloads

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The Server shoudl respond to all requests with a string
 var server = http.createServer(function (req, res) {
@@ -19,17 +20,29 @@ var server = http.createServer(function (req, res) {
     //Get the Query String
     var queryStringObject = parsedUrl.query;
 
-    //Send the Response
-    res.end('Hello World! \n');
-
     //Get the Path
     var method = req.method.toLowerCase();
 
     //Get the headers as an object
     var headers = req.headers;
 
-    //Log the requst path
-    console.log('Request recieved with these headers', headers);
+    //Get the Payload, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data',function(data){
+        buffer += decoder.write(data);
+    });
+
+    req.on('end',function(){
+        buffer += decoder.end();
+
+        //Send the Response
+        res.end('Hello World! \n');
+
+        //Log the requst path
+        console.log('Request recieved with this payload: ', buffer);
+
+    });
 
 });
 
